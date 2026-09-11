86a65c43270c4411df8d918af01cc9372a0fc904
Apia <96461146+apia46@users.noreply.github.com>
2026-09-12 00:30:19 +1200

    file reading

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use clap::Parser;
+use std::{fs, path::PathBuf};
 
 mod lexer;
 mod parser;
 
 #[derive(Parser, Debug)]
 #[command()]
-struct Args {
-    #[arg(short, long)]
-    input:String,
+enum Args {
+    Input {
+        #[arg(short)]
+        input:String
+    },
+    File {
+        #[arg(short)]
+        path: PathBuf
+    },
 }
 
 fn main() {
     let args = Args::parse();
-    let tokens = lexer::tokenize(&args.input).unwrap();
+    let input = match args {
+        Args::Input{input} => input,
+        Args::File{path} => fs::read_to_string(path).unwrap(),
+    };
+    let tokens = lexer::tokenize(&input).unwrap();
     let parse = parser::parse(tokens).unwrap();
     print!("{parse:?}");
 }
